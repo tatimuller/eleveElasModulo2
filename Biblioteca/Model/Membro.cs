45379aea667b99a8bc2e4ec62fdd20717ff5870e
Tatiane G. A. Muller <tatianegamuller@gmail.com>
2024-06-21 17:19:33 -0300

    Exercicio sobre encapsulamento

---
+++ Biblioteca/Model/Membro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.Model
+{
+    public class Membro
+    {
+        public string nome { get; set; }
+        private long id { get; set; }
+
+        internal void emprestarLivro()
+        {
+
+        }
+        private string getInfoMembros()
+        {
+            return "Informações detalhadas";
+        }
+
+    }
+}
